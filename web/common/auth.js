c58f12b6b6f57bd1b066efb0f8ada9f2450a7fd1
Hypeknight <willie_gerald1@yahoo.com>
2026-03-22 18:57:15 -0500

    ud1

--- web/common/auth.js
+++ web/common/auth.js
@@ -1,4 +1,4 @@
-/*window.LinkdNV2Auth = (() => {
+window.LinkdNV2Auth = (() => {
   function sb() {
     if (!window.LinkdNV2Supabase) throw new Error('Supabase client not initialized.');
     return window.LinkdNV2Supabase.getClient();
@@ -18,7 +18,7 @@
   }
   return { signIn, signUp, getUser, signOut };
 })();
-*/
+
 (function () {
   const db = window.LiveDB;
 
